27a3f34a3ac3f89ce0e1c6dec0e40cd21ad20ae7
Heloisa Fogaca do Nascimento <heloisafn@weg.net>
2024-03-12 14:17:03 -0300

    A príncipio tá tudo certo, amém!

--- src/main/java/com/crud/demo/FiltroAutenticacao.java
+++ src/main/java/com/crud/demo/FiltroAutenticacao.java
@@ -19,33 +19,33 @@ public class FiltroAutenticacao extends OncePerRequestFilter {
     private final CookieUtil cookieUtil = new CookieUtil();
     private final JwtUtil jwtUtil = new JwtUtil();
     private AutenticacaoService autenticacaoService;
-    private  SecurityContextRepository securityContextRepository;
+    private SecurityContextRepository securityContextRepository;
 
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-        // Busca e valida
-        Cookie cookie = cookieUtil.getCookie(request, "JWT");
-        String token = cookie.getValue();
-        String username = jwtUtil.getUsername(token);
+        if (!rotaPublica(request)) {
+            // Busca e valida
+            Cookie cookie = cookieUtil.getCookie(request, "JWT");
+            String token = cookie.getValue();
+            String username = jwtUtil.getUsername(token);
 
-        // Criação do usuário autenticado
-        UserDetails user = autenticacaoService.loadUserByUsername(username);
+            // Criação do usuário autenticado
+            UserDetails user = autenticacaoService.loadUserByUsername(username);
 
-        Authentication authentication = new UsernamePasswordAuthenticationToken(user, user.getPassword(), user.getAuthorities());
+            Authentication authentication = new UsernamePasswordAuthenticationToken(user, user.getPassword(), user.getAuthorities());
 
-        // Salvamento do usuário autenticadpp no Security Context
-        SecurityContext context = SecurityContextHolder.createEmptyContext(); // Criando um contexto vazio(só porque o authentication deu certo)
-        context.setAuthentication(authentication);
-        securityContextRepository.saveContext(context, request, response); // Salva o contexto com o objeto autenticado para que toda vez que for fazer uma requisição o próprio já reconheça que há um salvo, mantém o usuário ativo
-        // SecurityContextHolder.setContext(context);
-
-
+            // Salvamento do usuário autenticadpp no Security Context
+            SecurityContext context = SecurityContextHolder.createEmptyContext(); // Criando um contexto vazio(só porque o authentication deu certo)
+            context.setAuthentication(authentication);
+            securityContextRepository.saveContext(context, request, response); // Salva o contexto com o objeto autenticado para que toda vez que for fazer uma requisição o próprio já reconheça que há um salvo, mantém o usuário ativo
+            // SecurityContextHolder.setContext(context);
+        }
         // Continuação da requisição
         filterChain.doFilter(request, response);
     }
 
-    private boolean rotaPublica(HttpServletRequest request){
+    private boolean rotaPublica(HttpServletRequest request) {
         return request.getRequestURI().equals("/login") && request.getMethod().equals("POST");
     }
 }
--- src/main/java/com/crud/demo/SecurityConfig.java
+++ src/main/java/com/crud/demo/SecurityConfig.java
@@ -11,11 +11,13 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.context.HttpSessionSecurityContextRepository;
 import org.springframework.security.web.context.SecurityContextRepository;
 
@@ -23,6 +25,7 @@ import org.springframework.security.web.context.SecurityContextRepository;
 @AllArgsConstructor
 public class SecurityConfig {
     private final SecurityContextRepository repo; // armazena em memória o usuário da sessão
+    private final FiltroAutenticacao filtroAutenticacao = new FiltroAutenticacao();
 
     @Bean
     public SecurityFilterChain config(HttpSecurity http) throws Exception {
@@ -37,6 +40,10 @@ public class SecurityConfig {
         http.formLogin(Customizer.withDefaults());
 //        http.httpBasic(Customizer.withDefaults()); esse permite que ele fique no topo da página
         http.logout(Customizer.withDefaults());
+        http.sessionManagement(config->{
+            config.sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+        });
+        http.addFilterBefore(filtroAutenticacao, UsernamePasswordAuthenticationFilter.class);
         return http.build();
     }
 
